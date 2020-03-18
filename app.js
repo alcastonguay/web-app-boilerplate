867f6882944bb1ce6fd109c0196cba107dc91e92
Amanda Castonguay <alcastonguay@gmail.com>
2020-03-18 15:16:37 -0400

    Added bootstrap but style will not load.
    The bootstrap stylesheet is not accessible, does not seem to be exposed
    as a static directory under public.

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 // import Express.js (expressjs.com)
 const express = require('express');
+const path = require('path');
+
+// 80 is the default port for web applicaitons
+const port = 80;
 
 // initialize express app
 const app = express();
 
-// 80 is the default port for web applicaitons
-let port = 80;
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//expose public directory
+app.use(express.static('public'));
+
 
 // register get response for the root directory
-app.get('/', getResponse);
+app.get('/', renderResponse);
 
 // register server listener on the port
 app.listen(port);
 
 // function for building the get response 
 // TODO: extend this functionality and fix naming
-function getResponse(request, response){
-    response.send('Hello World!');
+function renderResponse(request, response){
+    response.render('index', {message: 'Hello World!'})
 }
 
