f5b76e91ceb56d89aaf9988c40b9e31952de257a
Amanda Castonguay <alcastonguay@gmail.com>
2020-03-20 14:24:03 -0400

    Bootstrap styles and javascript added and loading. Bootstrap container added to layout.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 // 80 is the default port for web applicaitons
-const port = 80;
+const port = 8080;
 
 // initialize express app
 const app = express();
@@ -13,11 +13,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 //expose public directory
-app.use(express.static('public'));
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 // register get response for the root directory
 app.get('/', renderResponse);
+
+app.get('/page', renderResponse);
 
 // register server listener on the port
 app.listen(port);
@@ -25,6 +26,13 @@ app.listen(port);
 // function for building the get response 
 // TODO: extend this functionality and fix naming
 function renderResponse(request, response){
-    response.render('index', {message: 'Hello World!'})
+    switch(request.path){
+        case '/':
+            response.render('index', {message: 'Hello World!'});
+            break;
+        case '/page':
+            response.render('page', {message: 'Here is page 2.'});
+            break;
+    }
 }
 
